d46868c20c9aa4ce76568ab9f70035ec32b40a78
Kerim <kerim.s.muhammetdurdyyew@gmail.com>
2026-08-17 17:53:48 +0500

    Täze üýtgeşme

--- settings.py
+++ settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'core/billing/static',
 ]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
